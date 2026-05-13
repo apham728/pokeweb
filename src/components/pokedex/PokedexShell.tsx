af2beda195a8c7a8d7b8f89fdcaea8aa9b40456f
Andre Pham <andrepham28@gmail.com>
2026-05-13 01:51:20 -0700

    Deleted files to focus on the closed UI of Pokedex

--- src/components/pokedex/PokedexShell.tsx
+++ src/components/pokedex/PokedexShell.tsx
@@ -1,21 +1,15 @@
-import { usePokedexIntro } from '../../hooks/usePokedexIntro'
 import { PokedexClosed } from './PokedexClosed'
-import { PokedexOpen } from './PokedexOpen'
 import styles from './pokedex.module.css'
 
 export function PokedexShell() {
-  const { state, isOpen } = usePokedexIntro()
-
   return (
     <section className={styles.stage}>
-      <article className={`${styles.pokedex} ${styles[state]}`} aria-label="Pokedex device">
+      <article className={styles.pokedex} aria-label="Closed Pokedex device">
         <div className={styles.topPlate} />
 
         <div className={styles.middleBand}>
           <PokedexClosed />
         </div>
-
-        <PokedexOpen isVisible={isOpen} />
 
         <div className={styles.bottomPlate} />
       </article>
